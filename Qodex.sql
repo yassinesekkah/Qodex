338a424b6f8ade04400b2a22fec86330793ca4f0
Yassine Sekkah <yassinesekkah11@gmail.com>
2025-12-19 23:40:37 +0100

    fix: exporter le fichier sql avec table creation requettes

--- Qodex.sql
+++ Qodex.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Hôte:                         127.0.0.1
--- Version du serveur:           8.4.3 - MySQL Community Server - GPL
+-- Version du serveur:           8.0.30 - MySQL Community Server - GPL
 -- SE du serveur:                Win64
--- HeidiSQL Version:             12.8.0.6908
+-- HeidiSQL Version:             12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -15,52 +15,83 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Listage de la structure de la base pour codex
-CREATE DATABASE IF NOT EXISTS `codex` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `codex`;
+-- Listage de la structure de la base pour qodex
+CREATE DATABASE IF NOT EXISTS `qodex` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `qodex`;
 
--- Listage des données de la table codex.categories : ~3 rows (environ)
-DELETE FROM `categories`;
-INSERT INTO `categories` (`id_categorie`, `nom_categorie`) VALUES
-	(1, 'Informatique'),
-	(2, 'Mathematiques'),
-	(3, 'Histoire');
+-- Listage de la structure de table qodex. categories
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id_categorie` int NOT NULL AUTO_INCREMENT,
+  `nom_categorie` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text COLLATE utf8mb4_general_ci,
+  PRIMARY KEY (`id_categorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Listage des données de la table codex.questions : ~0 rows (environ)
-DELETE FROM `questions`;
-INSERT INTO `questions` (`id_question`, `texte_question`, `reponse_correcte`, `points`, `id_quiz`) VALUES
-	(1, 'Quel est le résultat de 2+2?', '4', 5, 1),
-	(2, 'Résoudre x+5=10', '5', 10, 1),
-	(3, 'Quelle balise HTML pour un titre?', '<h1>', 5, 2),
-	(4, 'Comment changer la couleur en CSS?', 'color', 10, 2),
-	(5, 'En quelle année a commencé la Première Guerre mondiale?', '1914', 5, 3),
-	(6, 'Qui était le président des USA en 1945?', 'Harry Truman', 10, 3);
+-- Les données exportées n'étaient pas sélectionnées.
 
--- Listage des données de la table codex.quiz : ~0 rows (environ)
-DELETE FROM `quiz`;
-INSERT INTO `quiz` (`id_quiz`, `titre_quiz`, `description`, `id_categorie`, `duree_minutes`, `id_enseignant`) VALUES
-	(1, 'Quiz Algebre', 'Test sur les bases de l\'algèbre', 1, 30, 1),
-	(2, 'Quiz HTML/CSS', 'Test sur le développement web front-end', 2, 45, 5),
-	(3, 'Quiz Histoire Moderne', 'Questions sur l\'Histoire du XXème siècle', 3, 40, 1),
-	(5, 'sql_quiz', 'Description du quiz', 1, 60, 1);
+-- Listage de la structure de table qodex. questions
+CREATE TABLE IF NOT EXISTS `questions` (
+  `id_question` int NOT NULL AUTO_INCREMENT,
+  `texte_question` text COLLATE utf8mb4_general_ci NOT NULL,
+  `reponse_correcte` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `id_quiz` int NOT NULL,
+  `option1` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `option2` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `option3` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `option4` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_question`),
+  KEY `questions_ibfk_1` (`id_quiz`),
+  CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`id_quiz`) REFERENCES `quiz` (`id_quiz`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Listage des données de la table codex.resultats : ~0 rows (environ)
-DELETE FROM `resultats`;
-INSERT INTO `resultats` (`id_resultat`, `score`, `date_passage`, `id_etudiant`, `id_quiz`) VALUES
-	(1, 80, '2025-12-01 10:00:00', 3, 1),
-	(2, 90, '2025-12-01 11:00:00', 4, 1),
-	(3, 70, '2025-12-02 09:30:00', 5, 1),
-	(4, 85, '2025-12-02 10:00:00', 3, 2),
-	(5, 95, '2025-12-02 10:30:00', 4, 2);
+-- Les données exportées n'étaient pas sélectionnées.
 
--- Listage des données de la table codex.utilisateurs : ~5 rows (environ)
-DELETE FROM `utilisateurs`;
-INSERT INTO `utilisateurs` (`id_utilisateur`, `nom`, `email`, `motDePasse`, `ROLE`) VALUES
-	(1, 'Ahmed Benali', 'ahmed@gmail.com', '12345', 'enseignant'),
-	(2, 'Yassine Sekkah', 'yassine@gmail.com', '12345', 'etudiant'),
-	(3, 'Khadija Amrani', 'khadija@gmail.com', '12345', 'etudiant'),
-	(4, 'Omar Idrissi', 'omar@gmail.com', '12345', 'etudiant'),
-	(5, 'Malika Elhassani', 'sara@gmail.com', '12345', 'enseignant');
+-- Listage de la structure de table qodex. quiz
+CREATE TABLE IF NOT EXISTS `quiz` (
+  `id_quiz` int NOT NULL AUTO_INCREMENT,
+  `titre_quiz` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text COLLATE utf8mb4_general_ci,
+  `id_categorie` int NOT NULL,
+  `id_enseignant` int NOT NULL,
+  `duree_minutes` int DEFAULT '30',
+  PRIMARY KEY (`id_quiz`),
+  KEY `id_enseignant` (`id_enseignant`),
+  KEY `quiz_ibfk_1` (`id_categorie`),
+  CONSTRAINT `quiz_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id_categorie`) ON DELETE CASCADE,
+  CONSTRAINT `quiz_ibfk_2` FOREIGN KEY (`id_enseignant`) REFERENCES `utilisateurs` (`id_utilisateur`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table qodex. resultats
+CREATE TABLE IF NOT EXISTS `resultats` (
+  `id_resultat` int NOT NULL AUTO_INCREMENT,
+  `score` int NOT NULL,
+  `date_passage` datetime NOT NULL,
+  `id_etudiant` int NOT NULL,
+  `id_quiz` int NOT NULL,
+  PRIMARY KEY (`id_resultat`),
+  KEY `id_etudiant` (`id_etudiant`),
+  KEY `id_quiz` (`id_quiz`),
+  CONSTRAINT `resultats_ibfk_1` FOREIGN KEY (`id_etudiant`) REFERENCES `utilisateurs` (`id_utilisateur`),
+  CONSTRAINT `resultats_ibfk_2` FOREIGN KEY (`id_quiz`) REFERENCES `quiz` (`id_quiz`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table qodex. utilisateurs
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id_utilisateur` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  `motdepasse` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `role` enum('enseignant','etudiant') COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_utilisateur`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
